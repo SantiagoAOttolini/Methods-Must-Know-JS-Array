10b8d867995ff493469df7f000281b83f1299a76
santiago <santiago.ottolini97@hotmail.com>
2021-04-20 10:21:54 -0300

    Find

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const items = [
   { name: "Bike", price: 100 },
   { name: "TV", price: 200 },
-  { name: "Albun", price: 10 },
+  { name: "Album", price: 10 },
   { name: "Book", price: 5 },
   { name: "Phone", price: 500 },
   { name: "Computer", price: 1000 },
   { name: "Keyboard", price: 25 },
 ];
 
+//FILTER
 const filterItems = items.filter((item) => {
   return item.price >= 100;
 });
 
 console.log(filterItems);
+
+//MAP
+const mapItems = items.map((item) =>{
+    return item.price
+})
+
+console.log(mapItems)
+
+//FIND
+const findItems = items.find((item) =>{
+    return item.name === "Album"
+})
+
+console.log(mapItems)
+
